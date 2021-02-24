4375c4094471a280d63177b2b96733ab41e3fcdc
Homer White <homerhanumat@gmail.com>
2021-02-24 08:59:33 -0500

    add latest version

--- munge.R
+++ munge.R
@@ -1,7 +1,10 @@
 library(readxl)
 
-people <- read_excel("data/fakes.xlsx", sheet = "people")
-reports <- read_excel("data/fakes.xlsx", sheet = "reports")
+people<- read_excel("data/fakes.xlsx", sheet = "people", 
+                    col_types = c("numeric", "text", "text", 
+                                  "numeric", "text", "date"))
+reports <- read_excel("data/fakes.xlsx", sheet = "reports", 
+                      col_types = c("numeric", "text", "date"))
 participation <- read_excel("data/fakes.xlsx", sheet = "participation")
 
 save(people, file = "data/people.rda")
